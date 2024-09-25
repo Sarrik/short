d6fb44ae96c15933b7bf05a6c58071b983a752f1
Sarrik <lsmdragon1314@gmail.com>
2024-09-25 14:32:46 +0800

    Update create.js

--- functions/create.js
+++ functions/create.js
@@ -117,6 +117,13 @@ export async function onRequest(context) {
             })
         }
 
+        if(clientIP !== '120.77.32.171' || clientIP !== '47.106.241.49' || clientIP !== '47.106.212.46' || clientIP !== '39.108.134.118'){
+            return Response.json({ message: 'client error!' }, {
+                headers: corsHeaders,
+                status: 400
+            })
+        }
+
         // 生成随机slug
         const slug2 = slug ? slug : generateRandomString(7);
         // console.log('slug', slug2);
